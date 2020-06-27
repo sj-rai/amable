6c807eb01d1e88fa6ccc761f649f431c103460f5
Sanjoth J Rai <sanjr4279@gmail.com>
2020-06-27 22:38:22 +0530

    remove unit test for App.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,11 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+// TODO maybe add unit test later
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+//  import React from 'react';
+// import { render } from '@testing-library/react';
+// import App from './App';
+
+// test('renders learn react link', () => {
+//   const { getByText } = render(<App />);
+//   const linkElement = getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
